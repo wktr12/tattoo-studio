c373b6333c234671702c42829597f68d58169d5c
WKTR <68610628+wktr12@users.noreply.github.com>
2021-12-05 14:52:19 +0100

    update .js

--- main.js
+++ main.js
@@ -3,19 +3,38 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-const menuBtn = document.querySelector(".menu-btn");
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-  }
-});
+// const menuBtn = document.querySelector(".menu-btn");
+// let menuOpen = false;
+// menuBtn.addEventListener("click", () => {
+//   if (!menuOpen) {
+//     menuBtn.classList.add("open");
+//     menuOpen = true;
+//   } else {
+//     menuBtn.classList.remove("open");
+//     menuOpen = false;
+//   }
+// });
+
+// const btn = document.getElementById("menu-btn");
+// const menu = document.getElementById("mobile-menu");
+
+// function navToggle() {
+//   menu.classList.toggle("mobile-menu-visible");
+//   btn.classList.toggle("open");
+// }
+
+// function btnToggle() {
+//   var btn = document.getElementById("menu-btn");
+//   btn.classList.toggle("open");
+// }
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("mobile-menu");
 
 function navToggle() {
-  var menu = document.getElementById("mobile-menu");
+  btn.classList.toggle("open");
   menu.classList.toggle("mobile-menu-visible");
 }
+
+btn.addEventListener("click", navToggle);
+menu.addEventListener("click", navToggle);
