fcdaa0ce5f12ed8142fcb5e6bbfcb1a5ca73d9c7
WKTR <68610628+wktr12@users.noreply.github.com>
2021-12-09 19:11:18 +0100

    fix all

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+const header = document.querySelector("header");
+
 window.addEventListener("scroll", function () {
-  var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
